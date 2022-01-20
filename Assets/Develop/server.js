1424a8c5ae7097c1a33c7893213989e34788b233
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2022-01-20 22:44:45 +0000

    set up routes

---
+++ Assets/Develop/server.js
@@ -0,0 +1,15 @@
+
+const express = require ("express")
+const apiroutes =require ("./routes/apiroutes")
+const htmlroutes =require ("./routes/htmlroutes")
+
+const app = express()
+const PORT = process.env.PORT || 3000
+app.use(express.json ())
+app.use(express.urlencoded ({extended:true}))
+app.use(express.static("public"))
+app.use("/api",apiroutes)
+app.use("/", htmlroutes)
+app.listen(PORT, () => {
+    console.log (`listening on port ${PORT}`)
+})
