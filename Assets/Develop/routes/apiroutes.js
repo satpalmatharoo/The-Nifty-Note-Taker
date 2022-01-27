953e1cf386844ee977ec3abd5216ba0a4ed7f231
satpalmatharoo <84681197+satpalmatharoo@users.noreply.github.com>
2022-01-27 00:22:59 +0000

    import uuid

--- Assets/Develop/routes/apiroutes.js
+++ Assets/Develop/routes/apiroutes.js
@@ -1,6 +1,7 @@
 const router = require ("express").Router()
 const util = require ("util")
 const fs = require ("fs")
+const uuid = require ("uuid")
 
 
 const readFileAsync = util.promisify(fs.readFile)
@@ -14,4 +15,29 @@ router.get ("/notes", async(req,res) => {
     console.log (notes)
     res.json (parsednotes) 
 })
+
+
+router.post ("/notes", async(req,res) => {
+   
+    const notes = await readFileAsync ("db/db.json", "utf-8")
+    const parsednotes =JSON.parse(notes) 
+    const newNote = {...req.body, id:uuid()}
+    const newNotes = [...parsednotes,newNote];
+    writeFileAsync("db/db.json", JSON.stringify(newNotes))
+    res.json(newNotes)
+
+
+ })
+
+router.delete("/notes/:id", async(req,res) => {
+    const notes = await readFileAsync ("db/db.json", "utf-8")
+    const parsednotes =JSON.parse(notes)
+    const filterNotes =parsednotes.filter(note => note.id !==req.params.id)
+    writeFileAsync("db/db.json", JSON.stringify(filterNotes))
+    res.json({ok:true})
+
+})
+
 module.exports = router
+
+
